7a52156c505af9830d802fb7de02909ceac66500
Anand.Narayan <arnarayan@gmail.com>
2023-02-12 13:44:14 -0700

    Initial Commit

---
+++ LittleBlackBook/LittleBlackBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleBlackBookApp.swift
+//  LittleBlackBook
+//
+//  Created by Anand Narayan on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct LittleBlackBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
